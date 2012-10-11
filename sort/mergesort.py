a0e278d3091804454f5f520b13367c43e5957461
Sarath C <csarath@gmail.com>
2012-10-12 02:47:36 +0530

    bugfix

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -5,37 +5,35 @@ def merge(left, right):
 	print "Merging left " + str(left) + " and right " + str(right)
 	result = []
 	i,j = 0, 0
-	
+
 	while i < len(left) and j < len(right):
 		if left[i] < right[j]:
 			result.append(left[i])
-			i+=1
+			i= i + 1
 		else:
-			result.append(left[i])
-			j+=1
-	
+			result.append(right[j])
+			j = j + 1
+
 	# add the remaining
 	result += left[i:]
 	result += right[j:]
-	
-	print "Returning " + str(result)
-	return result	
-	
-# partition in to two and calls merge	
+	return result
+
+# partition in to two and calls merge
 def sort(lst):
 
 	if len(lst) < 2:
 		return lst
-	
+
 	middle = len(lst)/2
-	
+
 	left = sort(lst[:middle])
 	print "Processed left" + str(left)
-	
+
 	right = sort(lst[middle:])
 	print "Processed right" + str(right)
 
 	return merge(left,right)
 
-sort_items = [15,18,21,25,33,41,53,55]
+sort_items = [55,15,33,18,21,41,25,53]
 print sort(sort_items)
